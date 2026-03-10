c8eb768efd1a3cee97d3cf134797d89c98d52aea
Youma <zoor@netcourrier.com>
2026-03-10 16:02:50 +0100

    feat: add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import restart from 'vite-plugin-restart'
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    base: '/Starter-MMI2-2025/', // Base URL for GitHub Pages
     server:
     {
         host: true, // Open to local network and display URL
@@ -12,7 +13,7 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: false
     },
     plugins:
     [
